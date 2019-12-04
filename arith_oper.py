100d2f78804574b590a3180ac563d8dab01e8e33
huangxiaoqin <huangxiaoqin@qudian.com>
2019-12-04 16:03:14 +0800

    test

---
+++ arith_oper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+"""
+    @author:huang xiaoqin
+    @time: 2019/12/3
+    @desc:
+"""
+
+# 数据的基本运算
+# 整数相除需要注意
+print 3/2
+print 3.0/2
+
+# 指数
+print 2**3
+print 4**0.5
+
+# 取余
+print 5%2
