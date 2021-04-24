d921fa1ceb7408dcbe8959e10798476778f57bc7
duliri_hxq <duliri_hxq@duliri-hxqdeMacBook-Pro.local>
2021-04-24 21:52:57 +0800

    quick sort

--- data_structure/quick_sort.py
+++ data_structure/quick_sort.py
@@ -24,8 +24,19 @@ def quick_sort(num_list, i, j):
     quick_sort(num_list, i, right - 1)
     quick_sort(num_list, right + 1, j)
 
+def quick_sort1(num_list):
+    if len(num_list) < 2:
+        return num_list
+    else:
+        privot = num_list[0]
+        less = [i for i in num_list[1:] if i <= privot]
+        greater = [i for i in num_list[1:] if i > privot]
+        return quick_sort1(less) + [privot] + quick_sort1(greater)
+
 
 if __name__ == '__main__':
     a = [3,2,1,5,6,4]
     quick_sort(a, 0, len(a)-1)
     print(a)
+    a = [3, 2, 1, 5, 6, 4]
+    print(quick_sort1(a))
